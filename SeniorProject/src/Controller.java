fe439e6cbda9aea5be76486a0e5374c232797efb
David Hojnowski <hojnowdj@lemoyne.edu>
2017-09-27 23:54:25 -0400

    Began the Java code for the cotroling side. Only just bgan implementing MVC

---
+++ SeniorProject/src/Controller.java
@@ -0,0 +1,15 @@
+
+public class Controller {
+
+	View view;
+	Model model;
+	
+	public Controller(){
+		view = new View();
+		model = new Model();
+	}
+	
+	public void begin(){
+		
+	}
+}
---
+++ SeniorProject/src/SeniorProjectRunner.java
@@ -0,0 +1,13 @@
+
+public class SeniorProjectRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Controller control = new Controller();
+		control.begin();
+		
+
+	}
+
+}
