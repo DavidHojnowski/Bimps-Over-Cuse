378cbee395e78c99e52d53668c84b0b4a87c06d2
David Hojnowski <hojnowdj@lemoyne.edu>
2017-12-08 02:11:15 -0500

    More cleaning

--- SeniorProject/src/Controller.java
+++ SeniorProject/src/Controller.java
@@ -1,7 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+
 
 public class Controller implements ControlInterface, ActionListener{
 	//We implement keylistner becuase the key listner is going to be the main driver of the controller
